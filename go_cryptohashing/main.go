bd5ee374ad11230eb31552c393c8302fa63c6500
Aarol D'Souza <aarol.dsouza@gmail.com>
2025-06-25 13:15:07 +0530

    Implemented Caesar Cipher in Go (CLI)

---
+++ go_cryptohashing/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func cryptic(a string) string { //Caesar's Cipher
+	b := []byte(a) //converting to mutable string
+	for j := 0; j < len(a); j++ {
+		//first make sure all are in lower case (lower_case encryption)
+		if b[j] >= 65 && b[j] <= 90 {
+			b[j] += 32
+		} else if b[j] < 97 || b[j] > 122 {
+			continue
+		}
+		ch := b[j] - 'a'
+		ch++
+		ch %= 26
+		b[j] = ch + 'a'
+	}
+	return string(b)
+}
+
+func main() {
+	//Main fn
+	var s string
+	fmt.Print("Please Enter String to be Encrypted: ")
+	fmt.Scan(&s)
+	ans := cryptic(s)
+	red := color.New(color.FgRed).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	fmt.Printf("For the given input %s the encrypted string is %s", red(s), green(ans))
+}
